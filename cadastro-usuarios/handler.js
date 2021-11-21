98e533c46af172e35c6a2644d70a2ef24363616f
Vinicius Nicoceli <31939154+ViniciusNicoceli@users.noreply.github.com>
2021-11-21 14:06:49 -0300

    Async

--- cadastro-usuarios/handler.js
+++ cadastro-usuarios/handler.js
@@ -1,53 +1,113 @@
-'use strict';
-
+"use strict";
 const usuarios = [
-  {id: 1, nome: "Vinicius", dataNascimento: '1997-26-12'},
-  {id: 2, nome: "Joao", dataNascimento: '1983-09-16'},
-  {id: 3, nome: "Jose", dataNascimento: '1959-07-15'}
+  { id: 1, nome: "Maria", dataNascimento: "1984-11-01" },
+  { id: 2, nome: "Joao", dataNascimento: "1980-01-16" },
+  { id: 3, nome: "Jose", dataNascimento: "1998-06-06" },
 ];
 
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
+const { v4: uuidv4 } = require("uuid");
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient()
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const params = {
-  TableName: 'PACIENTES'
-}
+  TableName: "USUARIOS",
+};
 
 module.exports.listarUsuarios = async (event) => {
   try {
-    let data = await dynamoDb.scan(params).promise()
+    let data = await dynamoDb.scan(params).promise();
 
-    return{
+    return {
       statusCode: 200,
-      body: JSON.stringify(data)
-    }
+      body: JSON.stringify(data.Items),
+    };
   } catch (err) {
-    console.log('Error', err)
-    return{
+    console.log("Error", err);
+    return {
       statusCode: err.statusCode ? err.statusCode : 500,
       body: JSON.stringify({
-        error: err.name ? err.name : 'Exception',
-        message: err.message ? err.message : 'Unknown error'
-      })
-    }
+        error: err.name ? err.name : "Exception",
+        message: err.message ? err.message : "Unknown error",
+      }),
+    };
   }
 };
 
 module.exports.obterUsuario = async (event) => {
+  try {
+    const { usuarioId } = event.pathParameters;
 
-  const { usuarioId } = event.pathParameters
+    const data = await dynamoDb
+      .get({
+        ...params,
+        Key: {
+          usuario_id: usuarioId,
+        },
+      })
+      .promise();
 
-  const usuario = usuarios.find(usuario => usuario.id == usuarioId)
-
-  if(usuario === undefined){
-    return{
-      statusCode: 404,
-      body:JSON.stringify({error: 'Usuário não existe'},null,2)
+    if (!data.Item) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ error: "Usuário não existe" }, null, 2),
+      };
     }
+
+    const usuario = data.Item;
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(usuario, null, 2),
+    };
+  } catch (err) {
+    console.log("Error", err);
+    return {
+      statusCode: err.statusCode ? err.statusCode : 500,
+      body: JSON.stringify({
+        error: err.name ? err.name : "Exception",
+        message: err.message ? err.message : "Unknown error",
+      }),
+    };
   }
-  
-  return {
-    statusCode: 200,
-    body: JSON.stringify({usuario}, null, 2),
+};
+
+module.exports.cadastrarUsuario = async (event) => {
+  try {
+    const timestamp = new Date().getTime();
+
+    let dados = JSON.parse(event.body);
+
+    const { nome, data_nascimento, email, telefone } = dados;
+
+    const usuario = {
+      usuario_id: uuidv4(),
+      nome,
+      data_nascimento,
+      email,
+      telefone,
+      status: true,
+      criado_em: timestamp,
+      atualizado_em: timestamp,
+    };
+
+    await dynamoDb
+      .put({
+        TableName: "USUARIOS",
+        Item: usuario,
+      })
+      .promise();
+
+    return {
+      statusCode: 201,
+    };
+  } catch (err) {
+    console.log("Error", err);
+    return {
+      statusCode: err.statusCode ? err.statusCode : 500,
+      body: JSON.stringify({
+        error: err.name ? err.name : "Exception",
+        message: err.message ? err.message : "Unknown error",
+      }),
+    };
   }
 };
