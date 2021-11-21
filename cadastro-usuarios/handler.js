01c5100dcf5e29b4a1911803214bcbb2749d1e49
Vinicius Nicoceli <31939154+ViniciusNicoceli@users.noreply.github.com>
2021-11-21 13:40:27 -0300

    Alterado para integrar o banco com o DynamoDB

--- cadastro-usuarios/handler.js
+++ cadastro-usuarios/handler.js
@@ -6,34 +6,48 @@ const usuarios = [
   {id: 3, nome: "Jose", dataNascimento: '1959-07-15'}
 ];
 
+const AWS = require('aws-sdk');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
+const params = {
+  TableName: 'PACIENTES'
+}
+
 module.exports.listarUsuarios = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        usuarios
-      },
-      null,
-      2
-    ),
-  };
+  try {
+    let data = await dynamoDb.scan(params).promise()
+
+    return{
+      statusCode: 200,
+      body: JSON.stringify(data)
+    }
+  } catch (err) {
+    console.log('Error', err)
+    return{
+      statusCode: err.statusCode ? err.statusCode : 500,
+      body: JSON.stringify({
+        error: err.name ? err.name : 'Exception',
+        message: err.message ? err.message : 'Unknown error'
+      })
+    }
+  }
 };
 
 module.exports.obterUsuario = async (event) => {
 
-  const { usuarioId } = event.pathParameters;
+  const { usuarioId } = event.pathParameters
 
-  const usuario = usuarios.find(usuario => usuario.id == usuarioId);
+  const usuario = usuarios.find(usuario => usuario.id == usuarioId)
 
   if(usuario === undefined){
     return{
       statusCode: 404,
       body:JSON.stringify({error: 'Usuário não existe'},null,2)
     }
-  };
+  }
   
   return {
     statusCode: 200,
     body: JSON.stringify({usuario}, null, 2),
-  };
+  }
 };
